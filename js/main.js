e430e9ad60768d8b4f8af6230b589038b7995917
Edison Lee <edisonlee@edisonlee55.com>
2019-01-21 17:21:22 +0800

    Init

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+/**
+ * upload-your-waifu - main.js
+ * Copyright (c) 2019 MING-CHIEN LEE
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 var dirTree;
 var photoSwipeUpdateInterval;
 function getPageParams() {
